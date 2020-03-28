dc698cec619a87ec56ba883922607580a478857a
Þorvaður <thoddi@outlook.com>
2020-03-28 18:56:21 +0000

    Swagger bætt við

--- MovieList/MovieList/Startup.cs
+++ MovieList/MovieList/Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using MovieList.DAL.Models;
+using Microsoft.OpenApi.Models;
 
 namespace MovieList
 {
@@ -31,11 +32,24 @@ namespace MovieList
                 opt.UseInMemoryDatabase("MovieList"));
 
             services.AddControllers();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "MovieList API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                c.RoutePrefix = "swagger";
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
